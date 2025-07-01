64ab2189427f2f3b818c62eb9bb269a4e81891ae
Joakim Animate <joakimbwire23@gmail.com>
2025-07-01 20:39:16 +0300

    Add API endpoints for listings, bookings, and reviews

--- alx_travel_app/urls.py
+++ alx_travel_app/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -33,6 +33,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('listings.urls')),
     re_path(r'^$', lambda request: redirect('swagger/', permanent=False)),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
